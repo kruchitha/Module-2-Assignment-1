fa993bfa5c95d90d697927e28966072511692960
kruchitha <83858059+kruchitha@users.noreply.github.com>
2025-08-05 21:28:16 +0530

    Add files via upload

---
+++ 2Assignment.2task.py
@@ -0,0 +1,7 @@
+#Module 2: Basic Python Concepts
+# Task 2: Create a Personalized Greeting
+a = input("Enter your first name: ")
+b = input("Enter your last name: ")
+c = a + " " + b + "!"
+print("Hello", c, "Welcome to the Python program.")
+
